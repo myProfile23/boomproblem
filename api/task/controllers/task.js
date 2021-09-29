032121d0b5a652dedfab0eec96e2bf716726d08a
myProfile23 <78159465+myProfile23@users.noreply.github.com>
2021-09-29 11:56:26 +0300

    review

--- api/task/controllers/task.js
+++ api/task/controllers/task.js
@@ -1,14 +1,64 @@
 "use strict";
 
 const { sanitizeEntity } = require("strapi-utils");
+const mysql = require("mysql");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const db = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  password: "root",
+  database: "boomproblem",
+});
 
 module.exports = {
-  async updateComplete(ctx) {
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    const findTask = await strapi.services.task.findOne({ id });
+
+    const updateXp = ctx.request.body.xp;
+    const updateName = ctx.request.body.name;
+    const updateDescription = ctx.request.body.description;
+
+    if (updateName || updateDescription || updateXp) {
+      console.log(ctx.request.body);
+
+      findTask.name = updateName ? updateName : findTask.name;
+      findTask.description = updateDescription
+        ? updateDescription
+        : findTask.description;
+
+      if (updateXp) {
+        const diff =
+          updateXp > findTask.xp
+            ? -Math.abs(updateXp - findTask.xp)
+            : +Math.abs(updateXp - findTask.xp);
+        findTask.users.map((user) =>
+          db.query(
+            `update boomproblem.\`users-permissions_user\` set gained_xp = ${
+              user.gained_xp - diff
+            } where id = ${user.id}`,
+            function (err, result) {
+              if (err) throw err;
+              console.log(result);
+            }
+          )
+        );
+        findTask.xp = updateXp;
+
+        // console.log(findTask);
+      }
+      await strapi.services.task.update({ id }, findTask);
+      return "Task updated";
+    }
+    console.log("Missing fields");
+    return "Missing fields", findTask;
+  },
+  async completeTask(ctx) {
     let entity;
 
     const { id } = ctx.params;
@@ -17,9 +67,11 @@ module.exports = {
 
     const user = ctx.state.user;
 
+    // console.log(user);
+
     const strapiUser = await strapi
       .query("user", "users-permissions")
-      .findOne(user.id);
+      .findOne({ id: user.id });
 
     const completedTask = await strapi.services.task.findOne(ctx.params);
 
@@ -36,18 +88,27 @@ module.exports = {
         if (isCompleted.includes(true)) {
           return "Task is already completed";
         } else {
-          user.gained_xp = user.gained_xp + ctx.request.body.xp;
+          strapiUser.gained_xp = strapiUser.gained_xp + ctx.request.body.xp;
 
-          completedTask.users.push(user.id);
+          // console.log(strapiUser);
+
+          completedTask.users.push(strapiUser.id);
+          strapiUser.tasks.push(completedTask.id);
+
+          console.log(completedTask);
 
           entity = completedTask;
 
           entity = await strapi.services.task.update({ id }, completedTask);
           console.log("Task Completed");
 
-          await strapi.query("user", "users-permissions").update(user.id, user);
+          await strapi
+            .query("user", "users-permissions")
+            .update({ id: user.id }, strapiUser);
 
-          console.log(user);
+          console.log(strapiUser);
+
+          //console.log(user);
 
           return "Congrats your task is submitted";
         }
@@ -55,44 +116,42 @@ module.exports = {
         return "The submitted task is wrong! Check it and try again. Good luck!";
       }
     } else {
-      return "task misiing";
+      return "Task Missing";
     }
   },
-
-  async findOneComplete(ctx) {
+  async delete(ctx) {
     const { id } = ctx.params;
+    const findTask = await strapi.services.task.findOne({ id });
 
-    const user = ctx.state.user;
-
-    const strapiUser = await strapi
-      .query("user", "users-permissions")
-      .findOne(user.id);
-
-    //const completedTask = await strapi.services.task.findOne(ctx.params);
-
-    const findCompleted = strapiUser.tasks.map(
-      (task) => task.id === Number(id)
+    findTask.users.map((user) =>
+      db.query(
+        `update boomproblem.\`users-permissions_user\` set gained_xp = ${
+          user.gained_xp - findTask.xp
+        } where id = ${user.id}`,
+        function (err, result) {
+          if (err) throw err;
+          console.log(result);
+        }
+      )
     );
 
-    if (!findCompleted.includes(true)) {
-      return "This task it's not completed yet";
-    } else {
-      const entity = await strapi.services.task.findOne({ id });
-
-      return sanitizeEntity(entity, { model: strapi.models.task });
-    }
+    const entity = await strapi.services.task.delete({ id });
+    console.log(findTask);
+    console.log(entity);
+    return "Task deleted";
+    //sanitizeEntity(entity, { model: strapi.models.task });
   },
-  async findCompleted(ctx) {
+  async myCompletedTasks(ctx) {
     const user = ctx.state.user;
-
     const strapiUser = await strapi
       .query("user", "users-permissions")
-      .findOne(user.id);
-
+      .findOne({ id: user.id });
     console.log(strapiUser.tasks);
-
-    return strapiUser.tasks.map((completedTask) =>
-      sanitizeEntity(completedTask, { model: strapi.models.task })
-    );
+    return [
+      "Completed tasks",
+      strapiUser.tasks.map((completedTask) =>
+        sanitizeEntity(completedTask, { model: strapi.models.task })
+      ),
+    ];
   },
 };
